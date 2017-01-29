911697e445a70fc767a5a957abfca0bb293aef05
GuroKung <greenblood11@hotmail.com>
2017-01-29 19:39:16 +0700

    add youtube api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyC003XUydPr0cJmLkuLXWq4fiD3_z7v3MI';
+
 // Create New React Component
-const App = function () {
+const App = () => {
   return <div>Hi !</div>;
 }
 
 // Render Component to put it's HTML into a DOM
-ReactDOM.render(<App/>);
+ReactDOM.render(<App/>, document.querySelector('.container'));
