7dce5cf10bd06598bd212d76c1912c64e7ea7237
GuroKung <greenblood11@hotmail.com>
2017-02-02 02:01:12 +0700

    show video list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyC003XUydPr0cJmLkuLXWq4fiD3_z7v3MI';
 
 // Create New React Component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos : [] };
+
+    YTSearch({ key: API_KEY, term: 'hello' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Render Component to put it's HTML into a DOM
